9d2d07ae9959c2e96ae12a692762a8aaf1cd1638
Adrian <adrian.martinez89@gmail.com>
2021-11-30 00:41:21 +0100

    Esta es una prueba

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,7 +6,7 @@
             @include('search')
         </div>
     </div>
-
+    <!-- Añadiendo la estructura que va tener la vista -->
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="table-responsive">
